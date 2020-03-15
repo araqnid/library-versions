1198872da3e8212e42e5b66af402d8b32273ed31
Steve Haslam <araqnid@googlemail.com>
2020-03-15 04:40:08 +0000

    Split Java collecting flow easy/hard way into easy/hard way to call suspend functions

--- java/src/main/java/org/araqnid/libraryversions/java/JavaMain.java
+++ java/src/main/java/org/araqnid/libraryversions/java/JavaMain.java
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.Flow;
 import kotlinx.coroutines.flow.FlowKt;
 import kotlinx.coroutines.future.FutureKt;
 import org.araqnid.libraryversions.GradleResolver;
+import org.araqnid.libraryversions.MavenResolverKt;
 import org.araqnid.libraryversions.ZuluResolver;
 import org.jetbrains.annotations.NotNull;
 
@@ -26,15 +27,6 @@ import java.util.function.Function;
 
 public class JavaMain {
     private static <T> CompletableFuture<Unit> collectFlowEasyWay(Flow<? extends T> flow, CoroutineContext context, Consumer<? super T> consumer) throws InterruptedException {
-        return FutureKt.future(GlobalScope.INSTANCE, context, CoroutineStart.DEFAULT, (scope, cont) -> (
-                flow.collect((t, ignored) -> {
-                    consumer.accept(t);
-                    return Unit.INSTANCE;
-                }, cont)
-        ));
-    }
-
-    private static <T> CompletableFuture<Unit> collectFlowHardWay(Flow<? extends T> flow, CoroutineContext context, Consumer<? super T> consumer) throws InterruptedException {
         return callSuspendFunction(context, cont -> (
                 flow.collect((t, ignored) -> {
                     consumer.accept(t);
@@ -43,8 +35,21 @@ public class JavaMain {
         ));
     }
 
-    @SuppressWarnings("unchecked")
     public static <T> CompletableFuture<T> callSuspendFunction(CoroutineContext context, Function<? super Continuation<? super T>, ?> call) {
+        return FutureKt.future(GlobalScope.INSTANCE, context, CoroutineStart.DEFAULT, (scope, cont) -> call.apply(cont));
+    }
+
+    private static <T> CompletableFuture<Unit> collectFlowHardWay(Flow<? extends T> flow, CoroutineContext context, Consumer<? super T> consumer) throws InterruptedException {
+        return callSuspendFunctionHardWay(context, cont -> (
+                flow.collect((t, ignored) -> {
+                    consumer.accept(t);
+                    return Unit.INSTANCE;
+                }, cont)
+        ));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> CompletableFuture<T> callSuspendFunctionHardWay(CoroutineContext context, Function<? super Continuation<? super T>, ?> call) {
         CompletableFuture<T> future = new CompletableFuture<>();
 
         try {
@@ -82,11 +87,14 @@ public class JavaMain {
         delayFuture.join();
         System.out.println(Instant.now() + " - finished delay");
 
-        for (Flow<?> flow : List.of(
+        List<Flow<String>> flows = List.of(
                 FlowKt.flowOf("red", "blue", "orange"),
                 ZuluResolver.INSTANCE.findVersions(httpClient),
-                GradleResolver.INSTANCE.findVersions(httpClient)
-        )) {
+                GradleResolver.INSTANCE.findVersions(httpClient),
+                MavenResolverKt.mavenCentral("org.jetbrains.kotlin", "kotlin-stdlib").findVersions(httpClient)
+        );
+        Flow<String> aggregateFlow = FlowKt.flattenMerge(FlowKt.asFlow(flows), 4);
+        for (Flow<?> flow : List.of(aggregateFlow)) {
             try {
                 collectFlowEasyWay(flow, new CoroutineName("EasyWay"), item -> {
                     System.out.printf("Produced: %s%n", item);
