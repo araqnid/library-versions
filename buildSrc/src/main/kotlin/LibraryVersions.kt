47918a564fd22395abea68c67bd8c8d31bedede7
Steve Haslam <araqnid@googlemail.com>
2020-04-22 12:43:55 +0100

    Update Guava and Jetty

--- buildSrc/src/main/kotlin/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/LibraryVersions.kt
@@ -1,8 +1,7 @@
 object LibraryVersions {
-    const val jetty = "9.4.27.v20200227"
-    const val guava = "28.2-jre"
+    const val jetty = "9.4.28.v20200408"
+    const val guava = "29.0-jre"
     const val kotlinCoroutines = "1.3.5"
     const val slf4j = "1.7.30"
-    const val hamkrest = "1.7.0.0"
     const val jackson = "2.10.3"
 }
