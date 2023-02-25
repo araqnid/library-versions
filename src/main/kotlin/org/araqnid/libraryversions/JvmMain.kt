81c8866b8c32de7182087ce47efc75d547b383fa
Steve Haslam <araqnid@gmail.com>
2023-02-25 20:44:48 +0000

    Simplify main by writing directly rather than using Flow

--- src/main/kotlin/org/araqnid/libraryversions/JvmMain.kt
+++ src/main/kotlin/org/araqnid/libraryversions/JvmMain.kt
@@ -1,16 +1,13 @@
 package org.araqnid.libraryversions
 
-import com.google.common.base.Throwables
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.toList
 import org.araqnid.kotlin.argv.ArgParser
 import org.araqnid.kotlin.argv.ArgType
 import org.araqnid.kotlin.argv.parseArgv
 import java.net.http.HttpClient
 import kotlin.system.exitProcess
 
-@OptIn(FlowPreview::class)
 fun main(args: Array<String>) {
     val configFile = ArgParser("library-versions").run {
         val configFile by argument(ArgType.STRING, "Artifact list").optional()
@@ -28,21 +25,31 @@ fun main(args: Array<String>) {
 
         val httpClient = HttpClient.newHttpClient()
 
-        loadVersionResolvers(configFile).map { resolver ->
-                resolver.findVersions(httpClient)
-                    .map { version -> resolver to version }
-                    .catch { ex ->
+        val resolved = mutableMapOf<String, Deferred<Result<List<String>>>>()
+
+        for (resolver in loadVersionResolvers(configFile)) {
+            resolved[resolver.toString()] = async(Dispatchers.Default + CoroutineName(resolver.toString())) {
+                try {
+                    Result.success(resolver.findVersions(httpClient).toList())
+                } catch (ex: RuntimeException) {
+                    Result.failure(ex)
+                }
+            }
+        }
+
+        for ((resolver, deferredVersions) in resolved.entries.sortedBy { (resolver, _) -> resolver }) {
+            deferredVersions.await().fold(
+                { versions ->
+                    for (version in versions) {
                         println("- $resolver")
-                        Throwables.getStackTraceAsString(ex).lines().map { "  $it" }.forEach { println(it) }
+                        println("  $version")
                     }
-            }
-            .asFlow()
-            .flattenMerge()
-            .toList()
-            .sortedBy { (resolver, _) -> resolver.toString() }
-            .forEach { (resolver, version) ->
-                println("- $resolver")
-                println("  $version")
-            }
+                },
+                { ex ->
+                    println("! $resolver")
+                    println("! $ex")
+                }
+            )
+        }
     }
 }
