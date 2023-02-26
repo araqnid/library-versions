2a04f0b705a29f83a3057861ef3a3fad5a081777
Steve Haslam <araqnid@gmail.com>
2023-02-26 14:25:02 +0000

    Kotlin JVM compile task is just KotlinCompile

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ tasks {
         options.isDeprecation = true
     }
 
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>().configureEach {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
